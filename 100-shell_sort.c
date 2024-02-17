ead57ab29c2fca8aaba5057526668066c9e8ddd9
dexterski007 <mbouj007@gmail.com>
2024-02-17 21:38:29 +0000

    hey

---
+++ 100-shell_sort.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * gmax - gap max
+ *
+ * @size: size
+ * 
+ * Return: max gap
+ */
+
+size_t gmax(size_t size)
+{
+	size_t gap;
+
+	while (gap < size)
+		gap = (gap * 3) + 1;
+	return ((gap / 3) - 1);
+}
+
+
+/**
+ * shell_sort - shell sort
+ *
+ * @array: array
+ * @size: size
+ */
+
+void shell_sort(int *array, size_t size)
+{
+	size_t a, b, gap;
+	int tmp;
+
+	if (array == NULL || size == 0)
+		return;
+
+	for (gap = gmax(size); gap; gap = (gap - 1) / 3)
+	{
+		for (a = gap; a < size; a++)
+		{
+			tmp = array[a];
+			for (b = a; b > gap - 1 && array[b - gap] > tmp; b -= gap)
+				array[b] = array[b - gap];
+			array[b] = tmp;
+		}
+	}
+
+}
