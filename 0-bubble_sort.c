3c7435ccc2ca2d1e31d80fd9ca7961ef7a7003b0
dexterski007 <mbouj007@gmail.com>
2024-02-17 11:09:11 +0000

    hey

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - bubble sort
+ *
+ * @array: array
+ * @size: size
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t j, i, k = 0;
+	int tmp = 0;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (k = 0; k < size - i - 1; k++)
+		{
+			if (array[k] > array[k + 1])
+			{
+				tmp = array[k];
+				array[k] = array[k + 1];
+				array[k + 1] = tmp;
+				for (j = 0; j < size - 1; j++)
+				{
+					printf("%d, ", array[j]);
+				}
+				printf("%d\n", array[j]);
+			}
+		}
+	}
+}
