4eff65edc401f7644210ed5b6ba5caaad2df8a87
Dexterski007 <mbouj007@gmail.com>
2024-02-19 12:42:29 -0800

    hey

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,55 +1,98 @@
 #include "sort.h"
 
 /**
- * vexel - exchange lists
+ * vexel_forward - exchange lists forward
  *
- * @list1: list 1
- * @list2: list 2
+ * @list: list of nodes
+ * @pointer: 1st node
+ * @temp: 2nd node
  */
 
-void vexel(listint_t *list1, listint_t *list2)
+void vexel_forward(listint_t **list, listint_t **pointer, listint_t **temp)
 {
-	if (list1->prev)
-		list1->prev->next = list2;
-	if (list2->next)
-		list2->next->prev = list1;
-	list1->next = list2->next;
-	list2->prev = list1->prev;
-	list1->prev = list2;
-	list2->next = list1;
+	listint_t *prov = (*temp)->next;
+
+	if ((*temp)->prev != NULL)
+		(*temp)->prev->next = prov;
+	else
+		*list = prov;
+	prov->prev = (*temp)->prev;
+	(*temp)->next = prov->next;
+	if (prov->next != NULL)
+		prov->next->prev = *temp;
+	else
+		*pointer = *temp;
+	(*temp)->prev = prov;
+	prov->next = *temp;
+	*temp = prov;
+}
+
+/**
+ * vexel_backward - exchange lists backward
+ *
+ * @list: list of nodes
+ * @pointer: 1st node
+ * @temp: 2nd node
+ */
+
+void vexel_backward(listint_t **list, listint_t **pointer, listint_t **temp)
+{
+	listint_t *prov = (*temp)->prev;
+
+	if ((*temp)->next != NULL)
+		(*temp)->next->prev = prov;
+	else
+		*pointer = prov;
+	prov->next = (*temp)->next;
+	(*temp)->prev = prov->prev;
+	if (prov->prev != NULL)
+		prov->prev->next = *temp;
+	else
+		*list = *temp;
+	(*temp)->next = prov;
+	prov->prev = *temp;
+	*temp = prov;
 }
 
 
-
 /**
- * insertion_sort_list - insertion sort
+ * cocktail_sort_list - martini sort
  *
  * @list: list of numbs
  */
 
-void insertion_sort_list(listint_t **list)
+void cocktail_sort_list(listint_t **list)
 {
-	listint_t *point, *temp;
+	listint_t *pointer, *temp;
+	int flag = 1;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	point = (*list)->next;
-	while (point)
+	
+	for (pointer = *list; pointer->next != NULL;)
+		pointer = pointer->next;
+
+	while (flag == 1)
 	{
-		temp = point;
-		point = point->next;
-		while (temp && temp->prev)
+		flag = 0;
+		for (temp = *list; temp != pointer; temp = temp->next)
 		{
-			if (temp->prev->n > temp->n)
+			if (temp->n > temp->next->n)
 			{
-				vexel(temp->prev, temp);
-				if (!temp->prev)
-					*list = temp;
+				vexel_forward(list, &pointer, &temp);
 				print_list(*list);
+				flag = 1;
 			}
-			else
-				temp = temp->prev;
+		}
+		for (temp = temp->prev; temp != *list; temp = temp->prev)
+		{
+			if (temp->n < temp->prev->n)
+			{
+				vexel_backward(list, &pointer, &temp);
+				print_list(*list);
+				flag = 1;
+			}
 		}
 	}
 }
